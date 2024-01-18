6f1172f529d69a8987646b44b536b4ea53cfb590
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-01-18 21:51:52 +0100

    Test

--- utils/run_pr_summary.py
+++ utils/run_pr_summary.py
@@ -2,23 +2,18 @@ import os
 
 import requests
 
+REPO_NAME = os.getenv("REPO_NAME")
+PR_NUMBER = os.getenv("PR_NUMBER")
+github_token = os.getenv("GITHUB_TOKEN")
 
-def get_pr_diff(repo_name, pr_number, github_token):
+
+def get_pr_diff():
     """Fetches the diff of a specific PR from a GitHub repository."""
-    url = f"https://api.github.com/repos/{repo_name}/pulls/{pr_number}"
-    headers = {"Authorization": f"token {github_token}", "Accept": "application/vnd.github.v3.diff"}
+    url = f"https://api.github.com/repos/{REPO_NAME}/pulls/{PR_NUMBER}"
+    headers = {"Authorization": f"token {GITHUB_TOKEN}", "Accept": "application/vnd.github.v3.diff"}
     response = requests.get(url, headers=headers)
     return response.text if response.status_code == 200 else ""
 
 
 if __name__ == "__main__":
-    repo_name = os.getenv("REPO_NAME")
-    pr_number = os.getenv("PR_NUMBER")
-    github_token = os.getenv("GITHUB_TOKEN")
-
-    print(repo_name, pr_number, github_token)
-    pr_diff = get_pr_diff(repo_name, pr_number, github_token)
-    if pr_diff:
-        print(pr_diff)
-    else:
-        print("Failed to fetch PR diff")
+    pr_diff = get_pr_diff()
