9879ed53b4654b320565dcbeada6bb74c3d1c284
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-06-19 01:36:22 +0200

    Update update_markdown_code_blocks.py

--- utils/update_markdown_code_blocks.py
+++ utils/update_markdown_code_blocks.py
@@ -34,13 +34,19 @@ def format_code_with_ruff(temp_dir):
     """Formats all Python code files in the `temp_dir` directory using the 'ruff' linter tool."""
     try:
         # Run ruff check
-        subprocess.run(["ruff", "check", "--fix", "--extend-select", "I", str(temp_dir)], check=False)
+        subprocess.run(["ruff", "check", "--fix", "--extend-select", "I", str(temp_dir)], check=True)
         print("Completed ruff check ✅")
+    except Exception as e:
+        print(f"ERROR running ruff check ❌ {e}")
 
+    try:
         # Run ruff format
-        subprocess.run(["ruff", "format", "--line-length", "120", str(temp_dir)], check=True)
+        subprocess.run(["ruff", "format", "--line-length1", "120", str(temp_dir)], check=True)
         print("Completed ruff format ✅")
+    except Exception as e:
+        print(f"ERROR running ruff format ❌ {e}")
 
+    try:
         # Run docformatter
         subprocess.run(
             [
@@ -58,9 +64,8 @@ def format_code_with_ruff(temp_dir):
             check=True,
         )
         print("Completed docformatter ✅")
-
     except Exception as e:
-        print(f"ERROR running ruff: {e}")
+        print(f"ERROR running docformatter ❌ {e}")
 
 
 def generate_temp_filename(file_path, index):
