a8adf0f7d690cc6fbbf920bce69877a16a16440f
UltralyticsAssistant <web@ultralytics.com>
2024-10-15 20:24:17 +0000

    Auto-format by https://ultralytics.com/actions

--- actions/first_interaction.py
+++ actions/first_interaction.py
@@ -22,6 +22,7 @@ GITHUB_EVENT_NAME = os.getenv("GITHUB_EVENT_NAME")
 GITHUB_EVENT_PATH = os.getenv("GITHUB_EVENT_PATH")
 BLOCK_USER = os.getenv("BLOCK_USER", "false").lower() == "true"
 
+
 def get_event_content() -> Tuple[int, str, str, str, str, str, str]:
     """Extracts the number, node_id, title, body, username, and issue_type."""
     with open(GITHUB_EVENT_PATH) as f:
--- actions/utils/__init__.py
+++ actions/utils/__init__.py
@@ -3,12 +3,12 @@
 from .common_utils import remove_html_comments
 from .github_utils import (
     GITHUB_API_URL,
+    GITHUB_EVENT_NAME,
+    GITHUB_EVENT_PATH,
     GITHUB_HEADERS,
     GITHUB_HEADERS_DIFF,
     GITHUB_TOKEN,
     REPO_NAME,
-    GITHUB_EVENT_NAME,
-    GITHUB_EVENT_PATH,
     get_github_data,
     get_pr_diff,
     graphql_request,
