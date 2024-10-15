c7c8021b4d837bd1eb7b3cc7eee90606d21e8049
UltralyticsAssistant <web@ultralytics.com>
2024-10-15 22:16:43 +0200

    Update actions package

--- actions/__init__.py
+++ actions/__init__.py
@@ -17,3 +17,5 @@ from .first_interaction import main as first_interaction_main
 from .summarize_pr import main as summarize_pr_main
 from .summarize_release import main as summarize_release_main
 from .update_markdown_code_blocks import process_all_markdown_files
+
+__all__ = ['first_interaction_main', 'summarize_pr_main', 'summarize_release_main', 'process_all_markdown_files']
