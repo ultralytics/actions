84e3ccb21b6e13bdb28841ae7d66922c8176151f
UltralyticsAssistant <web@ultralytics.com>
2024-12-27 20:32:20 +0100

    Get commit authors
    Signed-off-by: UltralyticsAssistant <web@ultralytics.com>

--- actions/utils/openai_utils.py
+++ actions/utils/openai_utils.py
@@ -19,29 +19,52 @@ def get_completion(
     remove: List[str] = (" @giscus[bot]",),  # strings to remove from response
 ) -> str:
     """Generates a completion using OpenAI's API based on input messages."""
-    assert OPENAI_API_KEY or GITHUB_REPOSITORY.split("/")[0] == "ultralytics", "OpenAI API key is required."
-    url = "https://api.openai.com/v1/chat/completions"
-    headers = {"Authorization": f"Bearer {OPENAI_API_KEY}", "Content-Type": "application/json"}
+    assert (
+        OPENAI_API_KEY or GITHUB_REPOSITORY.split("/")[0] == "ultralytics"
+    ), "OpenAI API key is required."
+    headers = {
+        "Authorization": f"Bearer {OPENAI_API_KEY}",
+        "Content-Type": "application/json",
+    }
 
     content = ""
     max_retries = 2
-    for attempt in range(max_retries + 2):  # attempt = [0, 1, 2, 3], 2 random retries before asking for no links
-        data = {"model": OPENAI_MODEL, "messages": messages, "seed": int(time.time() * 1000)}
+    for attempt in range(
+        max_retries + 2
+    ):  # attempt = [0, 1, 2, 3], 2 random retries before asking for no links
+        data = {
+            "model": OPENAI_MODEL,
+            "messages": messages,
+            "seed": int(time.time() * 1000),
+        }
         r = requests.post(
-            url if OPENAI_API_KEY else "https://actions-public-dproatj77a-ew.a.run.app", headers=headers, json=data
+            "https://api.openai.com/v1/chat/completions"
+            if OPENAI_API_KEY
+            else "https://actions-public-dproatj77a-ew.a.run.app",
+            headers=headers,
+            json=data,
         )
         r.raise_for_status()
         content = r.json()["choices"][0]["message"]["content"].strip()
         for x in remove:
             content = content.replace(x, "")
-        if not check_links or check_links_in_string(content):  # if no checks or checks are passing return response
+        if not check_links or check_links_in_string(
+            content
+        ):  # if no checks or checks are passing return response
             return content
 
         if attempt < max_retries:
-            print(f"Attempt {attempt + 1}: Found bad URLs. Retrying with a new random seed.")
+            print(
+                f"Attempt {attempt + 1}: Found bad URLs. Retrying with a new random seed."
+            )
         else:
             print("Max retries reached. Updating prompt to exclude links.")
-            messages.append({"role": "user", "content": "Please provide a response without any URLs or links in it."})
+            messages.append(
+                {
+                    "role": "user",
+                    "content": "Please provide a response without any URLs or links in it.",
+                }
+            )
             check_links = False  # automatically accept the last message
 
     return content
