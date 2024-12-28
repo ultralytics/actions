118f8b636b604461e1672e7e43724ba6cbe2a1a8
UltralyticsAssistant <web@ultralytics.com>
2024-12-28 03:45:39 +0100

    Refactor to class
    Signed-off-by: UltralyticsAssistant <web@ultralytics.com>

--- actions/utils/__init__.py
+++ actions/utils/__init__.py
@@ -3,6 +3,7 @@
 from .common_utils import remove_html_comments
 from .github_utils import (
     GITHUB_API_URL,
+    Action,
     check_pypi_version,
     ultralytics_actions_info,
 )
@@ -10,6 +11,7 @@ from .openai_utils import get_completion
 
 __all__ = (
     "GITHUB_API_URL",
+    "Action",
     "check_pypi_version",
     "get_completion",
     "remove_html_comments",
