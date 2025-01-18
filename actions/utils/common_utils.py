93cd24fb591ce64f32017c89fa15086b4ed4ed47
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-01-18 16:02:31 +0100

    Update common_utils.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- actions/utils/common_utils.py
+++ actions/utils/common_utils.py
@@ -73,12 +73,8 @@ def is_url(url, check=True, max_attempts=3, timeout=2):
                         "Referer": "https://www.google.com/",
                         "Origin": "https://www.google.com/",
                     }
-                    return (
-                        requests.get(
-                            url, headers=headers, timeout=timeout, allow_redirects=True, stream=True
-                        ).status_code
-                        < 400
-                    )
+                    return True if (requests.head(url, headers=headers, timeout=timeout, allow_redirects=True).status_code < 400) else (requests.get(url, headers=headers, timeout=timeout, allow_redirects=True, stream=True).status_code < 400) 
+
                 except Exception:
                     if attempt == max_attempts - 1:  # last attempt
                         return False
