2344865e1b3a1e073f83a18f708f60116f7fb8fd
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-01-18 15:55:21 +0100

    Update common_utils.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- actions/utils/common_utils.py
+++ actions/utils/common_utils.py
@@ -60,14 +60,20 @@ def is_url(url, check=True, max_attempts=3, timeout=2):
                 try:
                     headers = {
                         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
-                        "Accept": "*/*",  # Wildcard for maximum compatibility
-                        "Accept-Language": "*",  # Wildcard for any language
-                        "Accept-Encoding": "*",  # Wildcard for any encoding
+                        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                        "Accept-Language": "en-US,en;q=0.9,es;q=0.8,zh-CN;q=0.7,zh;q=0.6",
+                        "Accept-Encoding": "gzip, deflate, br, zstd",
                         "sec-ch-ua": '"Chromium";v="132", "Google Chrome";v="132", "Not_A Brand";v="99"',
                         "sec-ch-ua-mobile": "?0",
                         "sec-ch-ua-platform": '"macOS"',
+                        "Sec-Fetch-Site": "none",
+                        "Sec-Fetch-Mode": "navigate",
+                        "Sec-Fetch-User": "?1",
+                        "Sec-Fetch-Dest": "document",
+                        "Referer": "https://www.google.com/",
+                        "Origin": "https://www.google.com/",
                     }
-                    return requests.get(url, headers=headers, timeout=timeout, allow_redirects=True).status_code < 400
+                    return requests.get(url, headers=headers, timeout=timeout, allow_redirects=True, stream=True).status_code < 400
                 except Exception:
                     if attempt == max_attempts - 1:  # last attempt
                         return False
