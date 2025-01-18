0b07c600597ab8817a536f294ee6010aa68fd553
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-01-18 15:44:07 +0100

    Update common_utils.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- actions/utils/common_utils.py
+++ actions/utils/common_utils.py
@@ -59,12 +59,15 @@ def is_url(url, check=True, max_attempts=3, timeout=2):
             for attempt in range(max_attempts):
                 try:
                     headers = {
-                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
                         "Accept": "*/*",  # Wildcard for maximum compatibility
                         "Accept-Language": "*",  # Wildcard for any language
                         "Accept-Encoding": "*",  # Wildcard for any encoding
+                        "sec-ch-ua": '"Chromium";v="132", "Google Chrome";v="132", "Not_A Brand";v="99"',
+                        "sec-ch-ua-mobile": "?0",
+                        "sec-ch-ua-platform": '"macOS"',
                     }
-                    return requests.head(url, headers=headers, timeout=timeout, allow_redirects=True).status_code < 400
+                    return requests.get(url, headers=headers, timeout=timeout, allow_redirects=True).status_code < 400
                 except Exception:
                     if attempt == max_attempts - 1:  # last attempt
                         return False
