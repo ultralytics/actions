13e6a7de6c0b1759148f1cb393b57f4acc10d8e0
UltralyticsAssistant <web@ultralytics.com>
2025-04-01 19:01:55 +0000

    Auto-format by https://ultralytics.com/actions

--- actions/utils/common_utils.py
+++ actions/utils/common_utils.py
@@ -8,7 +8,6 @@ from urllib import parse
 
 import requests
 
-
 REQUESTS_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
