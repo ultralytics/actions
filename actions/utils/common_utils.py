a803656150b757a99454478a0d6a7ecd954ba443
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-04-13 17:50:08 +0200

    Update common_utils.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- actions/utils/common_utils.py
+++ actions/utils/common_utils.py
@@ -243,7 +243,7 @@ def check_links_in_string(text, verbose=True, return_bad=False, replace=False):
 
 
 if __name__ == "__main__":
-    url = "https://www.sciencedirect.com/topics/computer-science/occlusion-handling"
+    url = "https://ultralytics.com/images/bus.jpg"
     string = f"This is a string with a [Markdown link]({url}) inside it."
 
     print(f"is_url(): {is_url(url)}")
