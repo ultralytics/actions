9823d44d7928454fc9afd4c449f8cb58b54ae4c1
UltralyticsAssistant <web@ultralytics.com>
2025-10-21 00:42:23 +0000

    Auto-format by https://ultralytics.com/actions

--- actions/dispatch_actions.py
+++ actions/dispatch_actions.py
@@ -3,9 +3,6 @@
 from __future__ import annotations
 
 import time
-
-
-
 from datetime import datetime
 
 from .utils import GITHUB_API_URL, Action
@@ -19,17 +16,7 @@ def get_pr_branch(event) -> str:
     """Gets the PR branch name."""
     pr_number = event.event_data["issue"]["number"]
     pr_data = event.get_repo_data(f"pulls/{pr_number}")
-    return pr_data.get("head", {}).get("ref", "main")        
-
-
-
-
-
-
-
-
-
-
+    return pr_data.get("head", {}).get("ref", "main")
 
 
 def trigger_and_get_workflow_info(event, branch: str) -> list[dict]:
