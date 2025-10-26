3166de3c73c9ddf139e19e42169fe62e23995b18
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-10-26 20:53:42 +0800

    Add explicit username to first interaction

--- actions/first_interaction.py
+++ actions/first_interaction.py
@@ -188,7 +188,7 @@ def main(*args, **kwargs):
         if event.should_skip_pr_author():
             return
 
-        print("Processing PR open with unified API call...")
+        print(f"Processing PR by @{username} open with unified API call...")
         diff = event.get_pr_diff()
         response = get_pr_open_response(event.repository, diff, title, username, label_descriptions)
 
