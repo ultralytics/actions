3dea01ec3615dea6a032c74d0a6df2ee5f8b74c2
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-11-01 17:31:45 +0800

    Parallelize docstring formatter

---
+++ actions/utils/example-failure.py
@@ -0,0 +1,11 @@
+
+
+def example():
+    """
+    Summary here.
+
+    Returns:
+        (str, openvino.runtime.Model | None): The OpenVINO model file path and openvino.runtime.Model object if export is
+            successful; otherwise, None.
+    """
+    return None
